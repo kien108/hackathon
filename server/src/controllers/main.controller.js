02ec66cd148c3466dff8a4a1e1349541c35d3d25
luuAG <duongvannam2001@gmail.com>
2022-04-02 22:03:03 +0700

    Nam 10:02PM

--- server/src/controllers/main.controller.js
+++ server/src/controllers/main.controller.js
@@ -1,4 +1,48 @@
+const csv = require("csv-parser");
+const fs = require("fs");
+
+const results = [];
+
 
 module.exports = {
-    // [POST] /api/v1/request
-}
+  // [POST] /api/request
+  request(req, res) {
+    try {
+        // lấy dữ liệu từ csv
+        fs.createReadStream("./uploads/file.csv")
+        .pipe(csv())
+        .on("data", (data) => results.push(data))
+        .on("end", () => {
+            let data = {};
+            console.log(results);
+            // chuyển đổi csv thành object có key là môn học
+            results.forEach(result => {
+                let firstField = Object.keys(result)[0];
+                let monHoc = result[firstField];
+
+                data[monHoc] = {
+                    "soTietMonHoc": result.soTietMonHoc,
+                    "giaoVien": result.giaoVien.split(',').map(gv => gv.trim())
+                };
+            });
+
+            // xoá file csv
+            fs.unlinkSync("./uploads/file.csv");
+        });
+
+        // lấy time và chuyển đổi theo ý Vinh
+        let time = req.body.key;
+        console.log('time:' + time);
+        
+        
+        
+        res.status(200).json({ status: "OK", message: "Successful" });
+    }catch(err) {
+        console.log(err.message);
+        res.status(500).json({ status: "Error", message: "Error at server" });
+    }
+
+    
+    
+  },
+};
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,17 @@
-
+const controller = require('../controllers/main.controller');
+const path = require('path');
+const multer  = require('multer')
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+	  cb(null, './uploads')
+	},
+	filename: function (req, file, cb) {
+	  cb(null, file.fieldname + path.extname(file.originalname))
+	}
+  })
+  
+  const upload = multer({ storage: storage })
 
 module.exports = (app) => {
-	app.use('/api/v1/request', ergiuwsvdnodsoindaiusvdurvdzuskb);
+	app.get('/api/request', upload.single('file'), controller.request);
 };
