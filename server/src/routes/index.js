f5c27f13e2f0cf57820201425ddd1a3ab49fa83b
kien108 <trungkien2062001@gmail.com>
2022-04-03 00:28:31 +0700

    send data sucessfully

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,17 +1,17 @@
-const controller = require('../controllers/main.controller');
-const path = require('path');
-const multer  = require('multer')
+const controller = require("../controllers/main.controller");
+const path = require("path");
+const multer = require("multer");
 const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-	  cb(null, './uploads')
-	},
-	filename: function (req, file, cb) {
-	  cb(null, file.fieldname + path.extname(file.originalname))
-	}
-  })
-  
-  const upload = multer({ storage: storage })
+   destination: function (req, file, cb) {
+      cb(null, "./uploads");
+   },
+   filename: function (req, file, cb) {
+      cb(null, file.fieldname + path.extname(file.originalname));
+   },
+});
+
+const upload = multer({ storage: storage });
 
 module.exports = (app) => {
-	app.get('/api/request', upload.single('file'), controller.request);
+   app.post("/api/request", upload.single("file"), controller.request);
 };
