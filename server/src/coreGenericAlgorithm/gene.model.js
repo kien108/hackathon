bd5dc7f025ddcfa00ceda16b706e33b8a53bc0e7
quanqvinh <nguyenquanqvinh@gmail.com>
2022-04-03 01:35:45 +0700

    fix

--- server/src/coreGenericAlgorithm/gene.model.js
+++ server/src/coreGenericAlgorithm/gene.model.js
@@ -1,5 +1,19 @@
 class Gene {
-    constructor(gene){
-        this.gene = gene;
+    constructor(_class, day, time, subject, teacher){
+        this.class = _class;
+        this.day = day;
+        this.time = time;
+        this.subject = subject;
+        this.teacher = teacher;
     }
-}
+    swap(gene) {
+        [this.class, gene.class] = [gene.class, this.class];
+        [this.day, gene.day] = [gene.day, this.day];
+        [this.time, gene.time] = [gene.time, this.time];
+        [this.subject, gene.subject] = [gene.subject, this.subject];
+        [this.teacher, gene.teacher] = [gene.teacher, this.teacher];
+    }
+    
+};
+
+module.exports = Gene;
